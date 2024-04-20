9da35c4c37de7e962d2e9cb917f1d99b1bfcadb4
Joao Lucio <jaum0711@gmail.com>
2024-04-20 13:21:45 -0300

    feat: button app bar update

--- lib/core/widgets/app_bar/bottom_app_bar_custom.dart
+++ lib/core/widgets/app_bar/bottom_app_bar_custom.dart
@@ -4,9 +4,14 @@ import 'package:go_router/go_router.dart';
 import 'package:task_hub/core/enums/enums.dart';
 import 'package:task_hub/core/styles/styles.dart';
 
-class BottomAppBarCustom extends StatelessWidget {
+class BottomAppBarCustom extends StatefulWidget {
   const BottomAppBarCustom({super.key});
 
+  @override
+  State<BottomAppBarCustom> createState() => _BottomAppBarCustomState();
+}
+
+class _BottomAppBarCustomState extends State<BottomAppBarCustom> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -38,21 +43,19 @@ class BottomAppBarCustom extends StatelessWidget {
             const Spacer(),
             IconButton(
               iconSize: 40,
-              icon: const Icon(
-                CupertinoIcons.home,
-              ),
+              color: _defineIconColor(EnumRoutes.home),
+              icon: const Icon(CupertinoIcons.home),
               onPressed: () {
-                context.go(EnumRoutes.home);
+                _redirect(EnumRoutes.home);
               },
             ),
             const Spacer(flex: 6),
             IconButton(
               iconSize: 42,
-              icon: const Icon(
-                CupertinoIcons.calendar,
-              ),
+              color: _defineIconColor(EnumRoutes.calendar),
+              icon: const Icon(CupertinoIcons.calendar),
               onPressed: () {
-                context.go(EnumRoutes.calendar);
+                _redirect(EnumRoutes.calendar);
               },
             ),
             const Spacer(),
@@ -60,5 +63,23 @@ class BottomAppBarCustom extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Color _defineIconColor(String widgetName) {
+    final route =
+        GoRouter.of(context).routerDelegate.currentConfiguration.fullPath;
+
+    if (route == widgetName) {
+      return AppColors.black;
+    } else {
+      return AppColors.black.withOpacity(0.4);
+    }
+  }
+
+  void _redirect(String route) {
+    /// used for update selected item color
+    setState(() {});
+
+    context.go(route);
   }
 }
